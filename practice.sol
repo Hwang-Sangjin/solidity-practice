a7b7ecdf8a43c9b21e7083017f8ef5b103920795
Hwang-Sangjin <aosdb5640@naver.com>
2022-01-18 16:46:09 +0900

    token list

--- practice.sol
+++ practice.sol
@@ -8,6 +8,9 @@ contract Practice {
     mapping (uint256 => address) public tokenOwner;
     mapping (uint256 => string) public tokenURIs;
 
+    //소유한 토큰 리스트
+    mapping(address => uint256[]) private _ownedTokens;
+
     //mint(tokenId, uri, owner)
     //transferFrom(from, to, tokenId) -> owner가 바뀌는 것 (from -> to)
 
@@ -16,13 +19,37 @@ contract Practice {
         //적힐 글자는 tokenURI
         tokenOwner[tokenId] = to;
         tokenURIs[tokenId] = tokenURI;
+
+        //add token to the List
+        _ownedTokens[to].push(tokenId);
+
+        return true;
     }
 
     function safeTransferFrom(address from, address to, uint256 tokenId) public{
         require(from == msg.sender, "from !=msg.sender");
         require(from == tokenOwner[tokenId], "you are not the owner of the token");
         
+        _removeTokenFromList(from, tokenId);
         tokenOwner[tokenId] = to;
+        _ownedTokens[to].push(tokenId);
+    }
+    function _removeTokenFromList(address from, uint256 tokenId) private{
+        //swap last token with deleting token
+        uint256 lastTokenIdex = _ownedTokens[from].length - 1;
+        for (uint256 i = 0; i < _ownedTokens[from].length; i++) {
+            if (tokenId == _ownedTokens[from][i]) {
+                // Swap last token with deleting token;
+                _ownedTokens[from][i] = _ownedTokens[from][lastTokenIdex];
+                _ownedTokens[from][lastTokenIdex] = tokenId;
+                break;
+            }
+        }
+        _ownedTokens[from].pop();
+    }
+
+    function ownedTokens(address owner) public view returns(uint256[] memory){
+        return _ownedTokens[owner];
     }
 
     function setTokenUri (uint256 id, string memory uri) public {
